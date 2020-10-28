122c881143803191037a3f4dcc5eb0d5a142e308
abhicv <abhijithcheruvery@gmail.com>
2020-10-28 13:46:32 +0000

    fixed blink.rs

--- examples/blink.rs
+++ examples/blink.rs
@@ -1,17 +1,21 @@
+
 use bcm2835_rs as bcm2835;
+use bcm2835_rs::{RPiGPIO, GPIOFunctionSelect, HIGH, LOW};
 
 fn main() {
 	if bcm2835::init() == 0 {
 		return;
 	}
+	
+	let LED = RPiGPIO::P11; //pin 17 of raspberry pi
 
-	bcm2835::gpio_fsel(bcm2835::RPiGPIO::P11, bcm2835::GPIOFunctionSelect::Input);
+	bcm2835::gpio_fsel(LED, GPIOFunctionSelect::Output);
 
 	loop {
-		bcm2835::gpio_write(bcm2835::RPiGPIO::P11, bcm2835::HIGH);
+		bcm2835::gpio_write(LED, HIGH);
 		bcm2835::delay(500);
 
-		bcm2835::gpio_write(bcm2835::RPiGPIO::P11, bcm2835::HIGH);
+		bcm2835::gpio_write(LED, LOW);
 		bcm2835::delay(500);
 	}
 
